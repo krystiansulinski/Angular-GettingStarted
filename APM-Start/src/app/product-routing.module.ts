6cf5897ff7df0945a6d83ef07203dac0c0af733a
krystiansulinski <krystian.sulinski@gmail.com>
2020-04-09 18:55:24 +0200

    Chapter 13

---
+++ APM-Start/src/app/product-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ProductListComponent } from './products/product-list.component';
+import { ProductDetailGuard } from './products/product-detail.guard';
+import { ProductDetailComponent } from './products/product-detail.component';
+
+@NgModule({
+  imports: [
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent },
+      {
+        path: 'products/:id',
+        canActivate: [ProductDetailGuard],
+        component: ProductDetailComponent,
+      },
+    ]),
+  ],
+  exports: [RouterModule]
+})
+export class ProductRoutingModule { }
